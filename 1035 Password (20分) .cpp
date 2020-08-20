2627cba5cc293f80254f22de59fc2a0aba16ca3a
yifang0-0 <51746239+yifang0-0@users.noreply.github.com>
2020-08-20 18:57:20 +0800

    sre.replace&&queue嵌套(未提交)
    关于queue 不是所有都能用下标和push_back 这里以后再看看

---
+++ 1035 Password (20分) .cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <stdio.h>
+#include <map>
+#include <queue>
+using namespace std;
+//用一些字符代替 1->@ 0->% l->L O->o
+//只显示要改的队伍 用map村
+map<string,string>output;
+queue <string> saved;
+string ifNeedModify(string a,int *flag){
+    for(int i=0;i<a.size();i++){
+        switch(a[i]){
+            case '1':a.replace(i,1,"@");*flag=1;break;
+            case '0':a.replace(i,1,"%");*flag=1;break;
+            case 'l':a.replace(i,1,"L");*flag=1;break;
+            case 'O':a.replace(i,1,"o");*flag=1;break;
+        }
+    }
+    return a;
+}
+//http://c.biancheng.net/view/1449.html 字符串的操作
+int main(){
+    int n;
+    cin>>n;
+    getchar();
+    for(int i=0;i<n;i++){
+        string name,code;
+        cin>>name>>code;
+        int flag=0;
+        code=ifNeedModify(code,&flag);
+        if(flag==1){
+            output[name]=code;
+            saved.push(name);
+            }
+    }
+    //map <string,string>::iterator it;
+    int it;
+    int size=saved.size();
+    if(size>0){
+        for(it=0;it<size;it++){
+        cout<<saved.front()<<' '<<output[saved.front()]<<endl;
+        saved.pop();
+        }
+    }
+    else printf("There is %d account and no account is modified",n);
+    
+}
