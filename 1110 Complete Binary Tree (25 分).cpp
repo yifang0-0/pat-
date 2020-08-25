5eecd50628a9668559cbe8e2cd2f5f0e07061d79
yifang0-0 <51746239+yifang0-0@users.noreply.github.com>
2020-08-25 21:28:36 +0800

    dfs做法（最简）

--- 1110 Complete Binary Tree (25 分).cpp
+++ 1110 Complete Binary Tree (25 分).cpp
@@ -1,99 +1,104 @@
 #include <iostream>
 #include <cstdio>
 #include <string>
-#include <vector>
+#include <stdlib.h>
 #include <queue>
 //判断是否完全二叉树
 //实际上可以根据节点标号的规律直接在生成的时候完成判断!
 //https://github.com/yifang0-0/pat-/edit/master/%23%20%E5%88%B7%E9%A2%98%E6%8C%87%E5%8D%97.md
 using namespace std;
 struct node{
-    int left,right,num;
+    int left,right;
 };
-int num=0;
 int maxID;
-queue <int>dfsqueue;
-vector<struct node> nodesInfo(22);
-void dfs(int nodeID){
-    nodesInfo[nodeID].num=num;
-    num++;
-    maxID=nodeID;
-    //cout<<maxID<<"<-nodeid "<<num-1<<endl;
+int curNum=0;
+int n=0;
+struct node nodesInfo[30];
+int dfs(int nodeID,int num){
+    if(curNum<num){
+         maxID=nodeID;
+         curNum=num;
+    }
     int left=nodesInfo[nodeID].left;
     int right=nodesInfo[nodeID].right;
-    //cout<<"left right"<<left<<" "<<right<<endl;
-    if(left!=-1)dfsqueue.push(left);
-    if(right!=-1)dfsqueue.push(right);
-    while(dfsqueue.size()>0){
-        int curid=dfsqueue.front();       
-        dfsqueue.pop();
-        dfs(curid);
-    }
-}
-int bTree=1;
-int ifBtree(int nodeID,vector<struct node> nodesInfo){
-    int left=nodesInfo[nodeID].left;
-    int right=nodesInfo[nodeID].right;
-    int num=nodesInfo[nodeID].num;
-    if(left!=-1){
-        if((num*2+1)!=nodesInfo[left].num){
-            bTree=0;
-            // cout<<"num*2+1"<<num*2+1<<" nodesInfo[left].num"<<nodesInfo[left].num<<endl;
-            // //return;
-            // cout<<nodeID<<"<-nodeid num->"<<num<<endl;
-            }
-        else ifBtree(left,nodesInfo);
-    }
-    if(right!=-1){
-        if((num*2+2)!=nodesInfo[right].num)
-        {
-            bTree=0;
-            // cout<<"num*2+2"<<num*2+2<<" nodesInfo[left].right"<<nodesInfo[right].num<<endl;
-            // cout<<nodeID<<"<-nodeid num->"<<num<<endl;
-
-        }
-        else ifBtree(right,nodesInfo);
-    }
+    if(left!=-1)dfs(left,num*2);
+    if(right!=-1)dfs(right,num*2+1);
 }
 
 int main(){
-    int n;
     cin>>n;
-    
-    int *used=new int[n];
+   // nodesInfo=(struct node*)malloc(sizeof(struct node)*(n+1));
+    int used[30];
     for(int i=0;i<n;i++){
-        used[i]=-1;
+        used[i]=0;
     }
-    getchar();
+    
     for(int i=0;i<n;i++){
-        char a,b;
-        scanf("%c %c\n",&a,&b);
+        string a,b;
+        cin>>a>>b;
         nodesInfo[i].left=-1;
         nodesInfo[i].right=-1;
-        if(a<='9'&&a>='0'){
-            used[a-'0']=1;
-            nodesInfo[i].left=a-'0';
+        
+        if(a!="-"){
+            int ia=stoi(a);
+            //used[ia]=1;
+            //nodesInfo[i].left=ia;
+             sscanf(a.c_str(), "%d", &nodesInfo[i].left);
+            used[nodesInfo[i].left]=1;
         }
-        if(b<='9'&&b>='0'){
-             used[b-'0']=1;
-            nodesInfo[i].right=b-'0';
+        if(b!="-"){
+             int ib=stoi(b);
+             //used[ib]=1;
+             //nodesInfo[i].right=ib;
+            sscanf(b.c_str(), "%d", &nodesInfo[i].right);
+          used[nodesInfo[i].right]=1;
         }
-
     }
-    int root;
+    int  root;
     for(int i=0;i<n;i++){
-        if(used[i]==-1){
+        if(used[i]==0){
             root=i;
             break;
         }
     }
-    dfs(root);
-    
-    ifBtree(root,nodesInfo);
-    if(bTree==1){
-        cout<<"Yes "<<maxID<<endl;
+    dfs(root,1);
+    //cout<<curNum<<endl;
+    if(curNum==n){
+        cout<<"YES"<<" "<<maxID;
     }
     else{
-        cout<<"No "<<root<<endl;
+        cout<<"NO "<<root;
     }
 }
+// #include <iostream>
+// using namespace std;
+
+// int E[20][2];  // 存储每个节点的左右孩子
+// int exist[20]; 
+// int maxIndex = 0, last;
+// void dfs(int u, int index)
+// {
+// 	if(u==-1) return;
+// 	if(index>maxIndex) maxIndex = index, last = u;
+// 	dfs(E[u][0], 2*index);  // 遍历左孩子
+// 	dfs(E[u][1], 2*index+1);  // 遍历右孩子
+// }
+// int main(int argc, char const *argv[])
+// {
+// 	int N; cin >> N;
+// 	for (int i = 0; i < N; i++)
+// 	{
+// 		string s1, s2; cin >> s1 >> s2;
+// 		int left=-1, right=-1;
+// 		if(s1!="-") sscanf(s1.c_str(), "%d", &left);
+// 		if(s2!="-") sscanf(s2.c_str(), "%d", &right);
+// 		exist[left] = exist[right] = 1;
+// 		E[i][0]=left, E[i][1]=right;
+// 	}
+// 	int root;
+// 	for (int i = 0; i < N; i++) if(exist[i]==0) root=i;
+// 	dfs(root, 1);
+// 	if(maxIndex==N) cout << "YES" << " " << last;
+// 	else cout << "NO" << " " << root;
+// 	return 0;
+// }
